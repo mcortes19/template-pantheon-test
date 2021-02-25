fdf9203b780cafc4d0272ba9bde8fc08a38e57b6
Marvin J. Cortés <marvin.cortes@gmail.com>
2021-02-24 23:38:46 -0600

    Initial commit.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,9 @@
 BRANCH=$1
 MESSAGE=$2
 REPO=$PANTHEON_GIT_REPO
+BUILDFOLDER=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1`
 CLONEFOLDER=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1`
+mv web $BUILDFOLDER
 git clone --branch $BRANCH $REPO $CLONEFOLDER || git clone $REPO $CLONEFOLDER
 cd $CLONEFOLDER
 git branch $BRANCH ; git checkout $BRANCH
@@ -10,10 +12,18 @@ shopt -s extglob
 rm -rf ./!(.git|.|..)
 cd ../
 composer install --no-dev --ignore-platform-reqs
-cp -r config drush modules settings themes vendor web $CLONEFOLDER/
-cp ./{.composer.json,composer.lock,composer.patches.json,package.json,package-lock.json,pantheon.yml} $CLONEFOLDER/
+find ./web/modules/contrib/ -name ".git" -exec rm -rf {} \;
+find ./web/themes/contrib/ -name ".git" -exec rm -rf {} \;
+find ./web/sites/all/assets/vendor/ -name ".git" -exec rm -rf {} \;
+find ./web/libraries/ -name ".git" -exec rm -rf {} \;
+find ./web/sites/all/libraries/ -name ".git" -exec rm -rf {} \;
+cp -r web $CLONEFOLDER/web
+cp -r content $CLONEFOLDER/
+mv vendor $CLONEFOLDER/
+cp pantheon.yml $CLONEFOLDER/
+cp composer.json $CLONEFOLDER/
+cp composer.lock $CLONEFOLDER/
 cd $CLONEFOLDER
-rm -rf ./web/sites/default/files
 # Change CUSTOMTHEME by your own theme folder.
 if [ -f ./themes/custom/CUSTOMTHEME/package.json ]; then
   cd ./themes/custom/CUSTOMTHEME
@@ -26,3 +36,8 @@ git commit -m "$MESSAGE"
 git push origin $BRANCH
 cd ../
 rm -rf $CLONEFOLDER
+rm -rf web
+mv $BUILDFOLDER web
+git checkout -- composer.lock
+composer install --ignore-platform-reqs
+git checkout -- composer.lock
